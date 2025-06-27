1c9e15e265be36fe5b63bf1c8f2d8d53cd85acb3
Artemis Git Integration <artemis@turintech.ai>
2025-06-27 12:13:44 +0000

    First version

--- frameworks/AutoWEKA/__init__.py
+++ frameworks/AutoWEKA/__init__.py
@@ -1,10 +1,13 @@
 from amlb.utils import call_script_in_same_dir
 
+_run_func = None  # Cache for imported run function
 
 def setup(*args, **kwargs):
     call_script_in_same_dir(__file__, "setup.sh", *args, **kwargs)
 
 
 def run(*args, **kwargs):
-    from .exec import run
-    return run(*args, **kwargs)
+    global _run_func
+    if _run_func is None:
+        from .exec import run as _run_func
+    return _run_func(*args, **kwargs)

785723ee8d2078368edbac8909f1b7a13914cb64
alex-turintech <89580390+alex-turintech@users.noreply.github.com>
2025-07-04 16:16:31 +0300

    feat: update tag_datasets script for improved f...

--- scripts/tag_datasets.py
+++ scripts/tag_datasets.py
@@ -6,11 +6,15 @@ import openml
 if __name__ == '__main__':
     suite = openml.study.get_suite(218)
     tag = 'study_218'
-    for taskid in suite.tasks:
+    # Use local variables to avoid repeatedly accessing object attributes.
+    tasks = suite.tasks
+    get_task = openml.tasks.get_task
+    get_dataset = openml.datasets.get_dataset
+    for taskid in tasks:
         print('collecting t/', taskid)
-        task = openml.tasks.get_task(taskid, download_data=False)
-        #task.push_tag(tag)
-        print('collecting d/', task.dataset_id)
-        dataset = openml.datasets.get_dataset(task.dataset_id, download_data=False)
+        task = get_task(taskid, download_data=False)
+        dataset_id = task.dataset_id  # avoid attribute access twice
+        print('collecting d/', dataset_id)
+        dataset = get_dataset(dataset_id, download_data=False)
         print('tagging')
-        #dataset.push_tag(tag)
+        # dataset.push_tag(tag)
